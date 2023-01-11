1a8c3b0f9945ec711e0d2def52c70ade7b919f06
Anastasia <a.grebenkina93@gmail.com>
2023-01-11 12:27:28 +0300

    new

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -19,6 +19,6 @@ class MobileBankApiTestV1 {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"));
+                .body("data", equalTo("some data"));
     }
 }
